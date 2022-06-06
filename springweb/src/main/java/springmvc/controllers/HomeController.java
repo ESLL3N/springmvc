506cc95c40ed05603c16bb1dda9af37aa222097c
ESLL3N <esllenescolar@gmail.com>
2022-06-06 17:47:40 +0000

    models

--- springweb/src/main/java/springmvc/controllers/HomeController.java
+++ springweb/src/main/java/springmvc/controllers/HomeController.java
@@ -2,6 +2,8 @@ package springmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import springmvc.models.Tabuada;
+
 @Controller
 
 public class HomeController {
@@ -12,13 +14,18 @@ public class HomeController {
 
     @RequestMapping("/tabuada")
     public String tabuada(Model model) {
-        int[] resultado = new int [10];
+       /*  int[] resultado = new int [10];
         for (int pos = 0; pos < 10;pos++){
             resultado[pos] = (pos + 1) * 2;
         }
         model.addAttribute("resultado" , resultado);
         return "tabuada";
-    }
+        */
+        Tabuada tab = new Tabuada (2);
+        model.addAttribute("resultado", tab.getResultados());
+        return "tabuada";
+
+    } 
 }
 
 
